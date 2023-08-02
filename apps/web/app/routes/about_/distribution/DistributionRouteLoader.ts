9a5da644a6b8d3360904ccd47ba35a62ddff643d
Michael Nelson <revnelson@gmail.com>
2023-08-01 22:32:05 -0700

    Increase supplier fetch limit.

--- apps/web/app/routes/about_/distribution/DistributionRouteLoader.ts
+++ apps/web/app/routes/about_/distribution/DistributionRouteLoader.ts
@@ -1,7 +1,7 @@
 import { LoaderFunction } from "@remix-run/node"
 
 export const loader: LoaderFunction = async ({ context: { payload } }) => {
-  const data = await payload.find({ collection: "suppliers" })
+  const data = await payload.find({ collection: "suppliers", limit: 999 })
 
   return data.docs
 }
